2790168dbf1bd14cf14a81d7af122a4fac337c72
Abhishek Srivastava <infiniticoder@gmail.com>
2021-07-06 13:13:56 +0530

    sanity

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
 
 function App() {
+  console.log(extendTheme)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChakraProvider>
+      </ChakraProvider>
     </div>
   );
 }
