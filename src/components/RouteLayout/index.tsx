b252d9a806597aad046d217bf7f92eeae75f638b
Abhishek Srivastava <abhishekmicosoft@gmail.com>
2021-08-08 16:33:25 +0530

    Header, Layout

--- src/components/RouteLayout/index.tsx
+++ src/components/RouteLayout/index.tsx
@@ -1,18 +1,11 @@
 import React from 'react'
 import { Box } from "@chakra-ui/react"
-<<<<<<< HEAD
 import { Header } from '../HeaderSidebar/Header'
-=======
->>>>>>> aec9592 (added routes)
 
 function RouteLayout() {
     return (
         <Box>
-<<<<<<< HEAD
             <Header />
-=======
-            Header
->>>>>>> aec9592 (added routes)
         </Box>
     )
 }
